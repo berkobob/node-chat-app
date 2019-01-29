2b9ef5a402410cea67b38e572a9c74d28f76ce81
BerkoBob <alever@omniis.com>
2019-01-29 15:17:31 +0000

    Emit newMessage on CreateMessage

--- server/server.js
+++ server/server.js
@@ -15,22 +15,17 @@ app.use(express.static(publicPath));
 io.on("connection", socket => {
     console.log("New user connected");
 
-    socket.emit("newMessage", {
-        from: "bob@example.com",
-        test: "Do you like my message",
-        createAt: 123
-    });
-
     socket.on("createMessage", message => {
         console.log("createMessage", message);
+        io.emit("newMessage", {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on("disconnect", socket => {
         console.log("User was disconnected");
-    });
-
-    socket.on("createEmail", newEmail => {
-        console.log("Create email", newEmail);
     });
 });
 
