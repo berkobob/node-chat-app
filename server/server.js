626f39990e3ae1e0906098338a3a915143fb053e
BerkoBob <alever@omniis.com>
2019-01-29 16:03:23 +0000

    create generateMessage util

--- server/server.js
+++ server/server.js
@@ -3,9 +3,9 @@ const http = require("http");
 const express = require("express");
 const socketIO = require("socket.io");
 
+const { generateMessage } = require("./utils/message");
 const publicPath = path.join(__dirname, "../public");
 const port = process.env.PORT || 3000;
-
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
@@ -15,30 +15,25 @@ app.use(express.static(publicPath));
 io.on("connection", socket => {
     console.log("New user connected");
 
-    socket.emit("newMessage", {
-        from: "Admin",
-        message: "Welcome to the chat app",
-        createdAt: new Date().getTime()
-    });
+    socket.emit(
+        "newMessage",
+        generateMessage("Admin", "Welcome to the chat app")
+    );
 
-    socket.broadcast.emit("newMessage", {
-        from: "Admin",
-        text: "A new user has joined",
-        createdAt: new Date().getTime()
-    });
+    socket.broadcast.emit(
+        "newMessage",
+        generateMessage("Admin", "A new user has joined")
+    );
 
     socket.on("createMessage", message => {
-        console.log("createMessage", message);
-        // io.emit("newMessage", {
-        //     from: message.from,
-        //     text: message.text,
-        //     createdAt: new Date().getTime()
-        // });
-        socket.broadcast.emit("newMessage", {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        });
+        console.log(
+            "createMessage",
+            generateMessage(message.from, message.text)
+        );
+        io.emit(
+            "newMessage",
+            generateMessage("Admin", "A new user has joined")
+        );
     });
 
     socket.on("disconnect", socket => {
